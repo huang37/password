ebe73f46897a4f9c3b6e84b4b3df2b20b6b3dfbe
Huang <huang37@mail2000.com.tw>
2018-03-03 11:58:30 +0800

    change sothing

--- password.py
+++ password.py
@@ -20,12 +20,14 @@
 password = 'a123456'
 n = 3
 while n > 0:
+  n = n - 1
   pwd = input('請輸入密碼：')
   if pwd == password:
     print('登入成功')
     break
   else:
-    n = n - 1
-    print('密碼錯誤！還有', n, '次機會')
+    print('密碼錯誤！')
+    if n > 0:
+      print('還有', n, '次機會')
 
   
